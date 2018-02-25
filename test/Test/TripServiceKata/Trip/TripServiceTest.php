5071ca45f159c71d3ae5feb715429d65032b325b
Miguel Ángel Sánchez Chordi <slayer.ma@gmail.com>
2018-02-25 13:44:20 +0100

    [GREEN] Test not return trips when users are not friends

--- test/Test/TripServiceKata/Trip/TripServiceTest.php
+++ test/Test/TripServiceKata/Trip/TripServiceTest.php
@@ -2,7 +2,9 @@
 
 namespace Test\TripServiceKata\Trip;
 
+use function is_array;
 use PHPUnit\Framework\TestCase;
+use TripServiceKata\Trip\Trip;
 use TripServiceKata\Trip\TripService;
 use TripServiceKata\User\User;
 
@@ -10,27 +12,49 @@ class TripServiceTest extends TestCase
 {
     private $tripService;
 
-    protected function setUp()
-    {
-        $this->tripService = new TestableTripService();
-    }
-
     /**
      * @test
      * @expectedException \TripServiceKata\Exception\UserNotLoggedInException
      */
     public function itShouldThrownAnExceptionWhenUserIsNotLoggedIn()
     {
-        $user = new User('Mangel');
-        $this->tripService->getTripsByUser($user);
+        $tripService = new TestableTripService(null);
+        $user = new User('Bob');
+        $tripService->getTripsByUser($user);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldNotReturnAnyTripsWhenUsersAreNotFriends()
+    {
+        $loggedInUser = new User('Joe');
+        $friend = new User('Jack');
+        $loggedInUser->addFriend($friend);
+        $japanTrip = new Trip();
+        $loggedInUser->addTrip($japanTrip);
+
+        $otherUser = new User('Marshall');
+        $tripService = new TestableTripService($loggedInUser);
+        $trips = $tripService->getTripsByUser($otherUser);
+
+        $this->assertTrue(is_array($trips));
+        $this->assertEmpty($trips);
     }
 }
 
 class TestableTripService extends TripService
 {
+    private $loggedInUser;
+
+    public function __construct(User $loggedInUser = null)
+    {
+        $this->loggedInUser = $loggedInUser;
+    }
+
     protected function getLoggedInUser()
     {
-        return null;
+        return $this->loggedInUser;
     }
 }
 
